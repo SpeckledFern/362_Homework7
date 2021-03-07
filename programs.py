62ab7b849594e527b4e38bee13cb7df2d8dcf28c
Sierra Freihoefer <freihoes@flip3.engr.oregonstate.edu>
2021-03-07 10:25:58 -0800

    Implemented if a year is divisible by 400 it still returns true

--- programs.py
+++ programs.py
@@ -14,7 +14,10 @@ def list_print():
 def leap(y):
    if(y%4 == 0):
       if (y%100 == 0):
-         return "false"
+         if(y%400 == 0):
+            return "true"
+         else:
+            return "false"
       else:
          return "true"
    else:
