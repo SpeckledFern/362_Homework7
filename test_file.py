d19591c009732d598d9bee0698328a783de5329e
Sierra Freihoefer <freihoes@flip3.engr.oregonstate.edu>
2021-03-07 09:12:20 -0800

    Wrote second test to determine if a multiple of 3 will output Fizz

--- test_file.py
+++ test_file.py
@@ -4,6 +4,8 @@ import programs
 class TestPrograms(unittest.TestCase):
    def test_output(self):
       self.assertEqual(programs.output_at(5), "Buzz")
+      self.assertEqual(programs.output_at(6), "Fizz")
+
 
 if __name__ == "__main__":
    unittest.main()
